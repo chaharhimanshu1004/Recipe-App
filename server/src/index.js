bf496296bc8c69b4cb47f0c736f9f2bc82f88b95
himanshu1004200 <chaharhimanshu03@gmail.com>
2023-12-10 14:15:29 +0530

    Recipe Routes

--- server/src/index.js
+++ server/src/index.js
@@ -2,12 +2,16 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import {userRouter} from './routes/users.js'
+import { recipesRouter } from "./routes/recipes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use('/auth',userRouter);
+app.use('/recipes',recipesRouter);
+
+
 
 mongoose.connect("mongodb+srv://hello:himanshu@cluster0.f8pggwb.mongodb.net/Cluster0?retryWrites=true&w=majority").then(console.log('db connected')).catch((err)=>console.log('db connection failed'));
 app.listen(3001,()=>console.log("Server started"));
