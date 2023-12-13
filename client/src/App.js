0bb3a25492e21baae913d3791292b653e3291f51
himanshu1004200 <chaharhimanshu03@gmail.com>
2023-12-13 13:00:36 +0530

    saved recipe page

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import { Home } from './pages/home';
-import { SavedRecipie } from './pages/saved-recipes';
+import { SavedRecipes } from './pages/saved-recipes';
 import { CreateRecipie } from './pages/create-recipes';
 import { Auth } from './pages/auth';
 import { Navbar } from './components/Navbar';
@@ -18,7 +18,7 @@ function App() {
           <Route path='/' element={<Home></Home>}></Route>
           <Route path='/auth' element={<Auth></Auth>}  ></Route>
           <Route path='/create-recipe' element={<CreateRecipie></CreateRecipie>} ></Route>
-          <Route path='/saved-recipes' element={<SavedRecipie></SavedRecipie>} ></Route>
+          <Route path='/saved-recipes' element={<SavedRecipes></SavedRecipes>} ></Route>
         </Routes>
       </BrowserRouter>
 
--- client/src/pages/saved-recipes.js
+++ client/src/pages/saved-recipes.js
@@ -1,5 +1,45 @@
-export const SavedRecipie=()=>{
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useGetUserID } from "../hooks/useGetUserID";
+
+export const SavedRecipes=()=>{
+    const [savedRecipes,setSavedRecipes] = useState([]);
+    const userID = useGetUserID();
+    useEffect(()=>{
+        const fetchSavedRecipe = async()=>{
+            try{
+                const response = await axios.get(`http://localhost:3001/recipes/savedRecipes/${userID}`);
+                setSavedRecipes(response.data.savedRecipes);
+            }catch(err){
+                console.log(err);
+            }
+        }
+
+        fetchSavedRecipe();
+    },[]);
+
+
+
+
+
     return(
-        <div>SavedRecipie</div>
+        <div>
+            <h1>Saved Recipes</h1>
+            <ul>
+                {savedRecipes.map((recipe)=>(
+                    <li key={recipe._id}>
+                        <div>
+                            <h2>{recipe.name}</h2>
+                        </div>
+                        <div className="instructions">
+                            <p>{recipe.instructions}</p>
+                        </div>
+                        <img src={recipe.imageUrl} alt={recipe.name} />
+                        <p>Cooking time : {recipe.cookingTime} (minutes)</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+        
     );
 }
